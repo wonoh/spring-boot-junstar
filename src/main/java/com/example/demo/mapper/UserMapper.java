ab2f2981b84a11e623ae6f28d0008ff8481806a5
wonoh <wldnsjdh44121@nate.com>
2018-11-13 15:21:26 +0900

    <db세팅>

--- src/main/java/com/example/demo/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.mapper;
-
-import java.util.List;
-
-import com.example.demo.model.UserVO;
-
-public interface UserMapper {
-	public List<UserVO> selectUserList() throws Exception;
-}
